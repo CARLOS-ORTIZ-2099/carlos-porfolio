0ef8fd912e09985a40c76db38d1cde48c2b0c19e
Carlos Eduardo Ortiz <carlosklinton.2016@gmail.com>
2025-01-06 17:21:51 -0500

    se añadio un archivo json para almacenar los skills y se hizo mejoras en el componente projects

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,13 +1,5 @@
 import styles from "./skills.module.css";
-import html from "/skills/html-logo.svg";
-import css from "/skills/css-logo.svg";
-import js from "/skills/js-logo.svg";
-import react from "/skills/react-logo.svg";
-import bootstrap from "/skills/bootstrap-logo.svg";
-import git from "/skills/git-logo.svg";
-import node from "/skills/node-logo.svg";
-import express from "/skills/express-logo.svg";
-import mongo from "/skills/mongo-logo.svg";
+import skills from "../../data/skills.json";
 
 export const Skills = () => {
   return (
@@ -17,87 +9,17 @@ export const Skills = () => {
       </h2>
 
       <div className={styles.skillsImagesContainer}>
-        <div className={styles.skills}>
-          <img
-            className={styles.imageSkill}
-            data-aos="fade-up-right"
-            src={html}
-            alt="image-skill"
-          />
-          <h3>html</h3>
-        </div>
-        <div className={styles.skills}>
-          <img
-            className={styles.imageSkill}
-            data-aos="fade-up-right"
-            src={css}
-            alt="image-skill"
-          />
-          <h3>css</h3>
-        </div>
-        <div className={styles.skills}>
-          <img
-            className={styles.imageSkill}
-            data-aos="fade-up-right"
-            src={js}
-            alt="image-skill"
-          />
-          <h3>javascript</h3>
-        </div>
-        <div className={styles.skills}>
-          <img
-            className={styles.imageSkill}
-            data-aos="fade-up-right"
-            src={react}
-            alt="image-skill"
-          />
-          <h3>react</h3>
-        </div>
-        <div className={styles.skills}>
-          <img
-            className={styles.imageSkill}
-            data-aos="fade-up-right"
-            src={bootstrap}
-            alt="image-skill"
-          />
-          <h3>bootstrap</h3>
-        </div>
-        <div className={styles.skills}>
-          <img
-            className={styles.imageSkill}
-            data-aos="fade-up-right"
-            src={git}
-            alt="image-skill"
-          />
-          <h3>git</h3>
-        </div>
-        <div className={styles.skills}>
-          <img
-            className={styles.imageSkill}
-            data-aos="fade-up-right"
-            src={node}
-            alt="image-skill"
-          />
-          <h3>node</h3>
-        </div>
-        <div className={styles.skills}>
-          <img
-            className={styles.imageSkill}
-            data-aos="fade-up-right"
-            src={express}
-            alt="image-skill"
-          />
-          <h3>express</h3>
-        </div>
-        <div className={styles.skills}>
-          <img
-            className={styles.imageSkill}
-            data-aos="fade-up-right"
-            src={mongo}
-            alt="image-skill"
-          />
-          <h3>mongo Db</h3>
-        </div>
+        {skills.map((skill, index) => (
+          <div key={index} className={styles.skills}>
+            <img
+              className={styles.imageSkill}
+              data-aos="fade-up-right"
+              src={skill.image}
+              alt="image-skill"
+            />
+            <h3>{skill.name}</h3>
+          </div>
+        ))}
       </div>
     </section>
   );
